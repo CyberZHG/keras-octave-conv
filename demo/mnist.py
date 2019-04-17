afa728a03b680b09db17bb9ad2411582d304e8d9
CyberZHG <CyberZHG@gmail.com>
2019-04-17 23:36:56 +0800

    Add mnist demo

--- demo/mnist.py
+++ demo/mnist.py
@@ -1,7 +1,7 @@
 import keras
 import keras.backend as K
 import numpy as np
-from keras.layers import Input, BatchNormalization, MaxPool2D, Dropout, Flatten, Dense
+from keras.layers import Input, BatchNormalization, MaxPool2D, Conv2D, Dropout, Flatten, Dense
 from keras.models import Model
 from keras.datasets import fashion_mnist
 from keras_octave_conv import OctaveConv2D
@@ -19,6 +19,7 @@ x_train, x_valid = x_train[:train_num, ...], x_train[train_num:, ...]
 y_train, y_valid = y_train[:train_num, ...], y_train[train_num:, ...]
 
 
+# Octave Conv
 inputs = Input(shape=(28, 28, 1))
 normal = BatchNormalization()(inputs)
 high, low = OctaveConv2D(64, kernel_size=3)(normal)
@@ -45,5 +46,37 @@ model.fit(
     validation_data=(x_valid, y_valid),
     callbacks=[keras.callbacks.EarlyStopping(monitor='val_acc', patience=2)]
 )
-dropout_score = model.evaluate(x_test, y_test)
-print('Accuracy: %.4f' % dropout_score[1])
+octave_score = model.evaluate(x_test, y_test)
+print('Accuracy of Octave: %.4f' % octave_score[1])
+
+
+# Normal Conv
+inputs = Input(shape=(28, 28, 1))
+normal = BatchNormalization()(inputs)
+conv = Conv2D(64, kernel_size=3)(normal)
+pool = MaxPool2D()(conv)
+conv = Conv2D(32, kernel_size=3)(pool)
+conv = Conv2D(16, kernel_size=3)(conv)
+pool = MaxPool2D()(conv)
+flatten = Flatten()(pool)
+normal = BatchNormalization()(flatten)
+dropout = Dropout(rate=0.4)(normal)
+outputs = Dense(units=10, activation='softmax')(dropout)
+model = Model(inputs=inputs, outputs=outputs)
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy'],
+)
+
+model.summary()
+model.fit(
+    x=x_train,
+    y=y_train,
+    epochs=10,
+    validation_data=(x_valid, y_valid),
+    callbacks=[keras.callbacks.EarlyStopping(monitor='val_acc', patience=2)]
+)
+normal_score = model.evaluate(x_test, y_test)
+print('Accuracy of Octave: %.4f' % octave_score[1])
+print('Accuracy of normal: %.4f' % normal_score[1])
